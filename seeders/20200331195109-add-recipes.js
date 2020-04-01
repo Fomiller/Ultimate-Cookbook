10dcb30347a048880671ea29eea900e2d3a79050
Forrest <Forrestmillerj@gmail.com>
2020-04-01 11:45:31 -0500

    Removed user and recipe id from seeds

--- seeders/20200331195109-add-recipes.js
+++ seeders/20200331195109-add-recipes.js
@@ -6,14 +6,12 @@ module.exports = {
         recipeName: 'Chocolate Cake',
         ingredients: 'Flour, Eggs, Milk, Chocolate powder, Butter',
         instructions: 'Cook for 45 min at 400 degrees.',
-        userID:'',
         createdAt: new Date(),
         updatedAt: new Date(),
       },{
         recipeName: 'Steak and Potatoes',
         ingredients: 'Steak, Potatoes, Corn',
         instructions: 'Cook the steak to a perfect medium rare, about 6 minutes. cook your baked potatoe to you liking. Steam your corn in the microwave. Plate. Done.',
-        userID:'',
         createdAt: new Date(),
         updatedAt: new Date(),
       }], {});
