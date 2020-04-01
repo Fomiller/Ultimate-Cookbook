4a61721a40bb62d4de5f3a9b5102881f660b5cb2
chizz <cbrittingham@gmail.com>
2020-04-01 13:26:17 -0500

    added code to set up handlebars

--- server.js
+++ server.js
@@ -11,9 +11,14 @@ app.use(express.static('public'));
 // app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 // app.use(passport.initialize());
 // app.use(passport.session());
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
 // Requiring our routes when we build them
-// require("./routes/html-routes.js")(app);
-// require("./routes/api-routes.js")(app);
+require('./routes/html-routes.js')(app);
+require('./routes/api-routes.js')(app);
 
 db.sequelize.sync({ force: true }).then(function() {
     app.listen(PORT, function() {
