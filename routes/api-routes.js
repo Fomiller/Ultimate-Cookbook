35f44a9f462c72e9e860baa259737d0a0704c1ef
Forrest <Forrestmillerj@gmail.com>
2020-04-02 13:13:00 -0500

    changed 'req.redirect() to 'res.redirect()'

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -18,7 +18,7 @@ module.exports = function(app){
 
     app.get('/logout', function(req,res) {
         req.logout();
-        req.redirect('/');
+        res.redirect('/');
     });
 
     // create a user
