7aa51bc624a9ed60c01857a3ebd7822171424d65
Forrest <Forrestmillerj@gmail.com>
2020-04-02 13:01:22 -0500

    formating

--- config/passport.js
+++ config/passport.js
@@ -3,35 +3,35 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
 db = require('../models');
+
 passport.use(new LocalStrategy(
-    {
-        usernameField: 'email'
-    },
-    function(email, password, done) {
-        db.User.findOne({
-            where: {
-                email: email
-            },
-        }).then(function(dbUser) {
-            // if there is no user with the given email
-            if (!dbUser) {
-                return done(null, false {
-                    message: 'Incorrect email.'
-                });
-						}
-						// if there is a user with the correct given email but the passowrd is wrong
-						else if (!dbUser.validPassword(password)) {
-							return done(null, false {
-								message: 'Incorrect password.'
-							});
-						}
-						// if none of the aboive, return user... User email and password are correct.
-						return done(null, dbUser);
-        });
-    }
+	{
+		usernameField: 'email'
+	},
+	function(email, password, done) {
+		db.User.findOne({
+			where: {
+				email: email
+			},
+		}).then(function(dbUser) {
+			// if there is no user with the given email
+			if (!dbUser) {
+				return done(null, false, {
+					message: 'Incorrect email.'
+				});
+				// if there is a user with the correct given email but the passowrd is wrong
+			}	else if (!dbUser.validPassword(password)) {
+					return done(null, false, {
+						message: 'Incorrect password.'
+					});
+				}
+			// if none of the above, return user... User email and password are correct.
+			return done(null, dbUser);
+		});
+	}
 ));
 
-// Boiler plates that serialize and deserialize the user 
+// Boiler plates that serialize and deserialize the user
 // makes every thing work. Do not touch!
 passport.serializeUser(function(user, cb) {
 	cb(null, user);
@@ -40,3 +40,5 @@ passport.serializeUser(function(user, cb) {
 passport.deserializeUser(function(obj, cb) {
 	cb(null, obj);
 });
+
+module.exports = passport;
