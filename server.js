0aba367419d83a35da4d161577a5b4e1f7d0ebb1
Forrest <Forrestmillerj@gmail.com>
2020-04-01 20:53:05 -0500

    required passport. redefined variables as constants.

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require('express');
-//require passport
-var PORT = process.env.PORT || 8080;
-var db = require('./models');
-var app = express();
+const exphbs = require('express-handlebars');
+const passport = require('./config/passport');
+const db = require('./models');
+
+// create app with express
+const app = express();
+
+// define port
+const PORT = process.env.PORT || 8080;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 // this code was used for passport session
 // app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-// app.use(passport.initialize());
+app.use(passport.initialize());
 // app.use(passport.session());
 
-var exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
