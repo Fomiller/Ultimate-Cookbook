fe57ba7d68777458847c32b49d5337f1e728a461
Forrest <Forrestmillerj@gmail.com>
2020-04-02 13:47:05 -0500

    updated

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,11 +1,22 @@
-const path = require('path');
+// const path = require('path');
 
-// require custom middleware isAuthtenticated 
-var isAuthtenticated = require('../config/middleware/isAuthenticated');
+// require custom middleware isAuthenticated
+var isAuthenticated = require('../config/middleware/isAuthenticated');
 
 module.exports = function(app){
     // login in page
     app.get('/', function(req, res){
+        if (req.user) {
+            res.render('members');
+        }
+        res.render('signup');
+    });
+
+    app.get('/login', function(req, res) {
+        console.log(req.user);
+        if (req.user) {
+            res.redirect('/members');
+        }
         res.render('index');
     });
 
@@ -15,7 +26,7 @@ module.exports = function(app){
     });
 
     // members page, served after successful login
-    app.get('/members', function(req, res) {
+    app.get('/members', isAuthenticated, function(req, res) {
         res.render('members');
     });
 
