4d83394d1815caac76b9ee3fd00d06eed30c36cb
Forrest <Forrestmillerj@gmail.com>
2020-04-03 09:38:18 -0500

    added routes for all recipes search page and user profile.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -29,22 +29,34 @@ module.exports = function(app){
     // members page, served after successful login
     // working correctly. if i restart the server and go to the root and then try to go to '/members' i am redirected to '/'.
     // this is made possible by the isAuthenticated middleware.
-  
+
     app.get('/members', isAuthenticated, function(req, res) {
       console.log(req.session);
       res.render('members');
     });
-  
+
     //not used route
     app.get('/recipes', function(req, res){
         console.log('found the page');
         res.json('okii');
     });
-  
+
     //this should be in api routes
     app.get('api/recipes', function(req, res){
       console.log('found the page');
       res.json('okii');
     });
+
+    // user profile
+    app.get('/profile', isAuthenticated, function(req, res) {
+      console.log('profile log ', req.session);
+      res.render('user-profile');
+    });
+
+    // search recipes
+    app.get('/all-recipes', function(req, res) {
+      res.render('browse-recipe');
+      console.log('All recipes page');
+    })
 };
 
