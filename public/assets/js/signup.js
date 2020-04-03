8233020f76f618c06ce9f4efbc929fd46166f2e7
chizz <cbrittingham@gmail.com>
2020-04-03 08:00:52 -0500

    cleaned up eslint errors

--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-$(document).ready(function() {
-    let signupForm = $('form.signup');
-    let emailInput = $('input#email-input');
-    let passwordInput = $('input#password-input');
-
-    // creates and alert with the err Message
-    // set fade in time
-    function handleLoginErr(err) {
-        $('#alert .msg').text(err.responseJSON);
-        $('#alert').fadeIn(500);
-    }
-    // signup the user if email and password are present.
-    function signUpUser(email, password) {
-        $.post('api/signup', {
-            email: email,
-            password: password
-        }).then(function() {
-            // if signup is successful then the window is redirected to the /members endpoint.
-            window.location.replace('/members');
-        }).catch(handleLoginErr);
-    }
-
-    // check to see if email and password are not blank
-    signupForm.on('submit', function(event) {
-        event.preventDefault();
-        let userData = {
-            email: emailInput.val().trim(),
-            password: passwordInput.val().trim()
-        };
-        if (!userData.email || !userData.password) {
-            return;
-          }
-        //if both email and password are not null then signupuser()
-          signUpUser(userData.email, userData.password);
-          emailInput.val('');
-          passwordInput.val('');
-    });
-=======
 $(document).ready(function() {
 	let signupForm = $('form.signup');
 	let emailInput = $('input#email-input');
@@ -87,5 +48,4 @@ $(document).ready(function() {
 				firstNameInput.val('');
 				lastNameInput.val('');
 	});
->>>>>>> de65f3573262f8ba2a3e1370ad2d599bfd71d295
 });
