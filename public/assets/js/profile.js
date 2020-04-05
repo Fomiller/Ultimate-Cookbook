73c30c2ce6b8988bb85884c88305d9daa3d7b17d
Forrest <Forrestmillerj@gmail.com>
2020-04-05 13:52:19 -0500

    organized and commented functions

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -10,15 +10,16 @@ $(document).ready(function() {
   let descriptionInput = $('#description');
   let chefCommentsInput = $('#chef-comments');
   let addRecipeForm = $('form.add-recipe');
+  // Buttons for updating Recipes
   let deleteBtn = $('.del-btn');
   let editBtn = $('.edit-btn');
   let saveBtn = $('.save-btn');
+  // variables for updating Bio
 
-
-
+  // set all save buttons to display none
   saveBtn.hide();
 
-
+  // delete Recipe
   function deleteRecipe(id) {
     $.ajax({
       method: 'DELETE',
@@ -29,60 +30,24 @@ $(document).ready(function() {
     });
   }
 
-  $.get('/api/userData').then( function(data){
-    // console.log('userdata', data.id);
-    usernameField.text(data.username);
-    firstnameField.text(data.firstName);
-    lastnameField.text(data.lastName);
-    bioField.text(data.bio);
-    return UserId = data.id;
-  });
+  // update Recipe
+  function updateRecipe(recipe) {
+    $.ajax({
+      method: 'PUT',
+      url: '/api/recipes',
+      data: recipe
+    }).then(function(data) {
+      window.location.replace('/profile');
+    });
+  }
 
-  addRecipeForm.on('submit', function(event) {
-    event.preventDefault();
-    let recipeData = {
-      recipeName: recipeNameInput.val().trim(),
-      ingredients:ingredientsInput.val().trim(),
-      instructions:instructionsInput.val().trim(),
-      description:descriptionInput.val().trim(),
-      chefComments:chefCommentsInput.val().trim(),
-    };
-
-    if (!recipeData.recipeName || !recipeData.ingredients || !recipeData.instructions || !recipeData.description) {
-      alert('please fill out the form completely');
-      return;
-    }
-
-    function addRecipe(recipeName, ingredients, instructions, description, chefComments) {
-      $.post('/api/add-recipe', {
-        recipeName: recipeName,
-        ingredients: ingredients,
-        instructions: instructions,
-        description: description,
-        chefComments: chefComments,
-      }).then(function(data) {
-        // reload page
-        console.log('profile.js');
-        window.location.replace('/profile');
-      }).catch(err => res.status(401).json(err));
-    }
-
-    addRecipe(recipeData.recipeName, recipeData.ingredients, recipeData.instructions, recipeData.description, recipeData.chefComments);
-    // reset form values to empty
-    recipeNameInput.val('');
-    ingredientsInput.val('');
-    instructionsInput.val('');
-    descriptionInput.val('');
-    chefCommentsInput.val('');
-  });
-
-
-
+  // Delete Recipe button
   deleteBtn.on('click', function() {
     let btnId = $(this).data('id');
     deleteRecipe(btnId);
   });
 
+  // edit Recipe Button
   editBtn.on('click', function() {
     let btnId = $(this).data('id');
     let save = $(this).siblings('.save-btn');
@@ -99,17 +64,7 @@ $(document).ready(function() {
     save.toggle();
   });
 
-  function updateRecipe(recipe) {
-    $.ajax({
-      method: 'PUT',
-      url: '/api/recipes',
-      data: recipe
-    }).then(function(data) {
-      window.location.replace('/profile');
-    });
-  }
-
-
+  // update Recipe Button
   saveBtn.on('click', function() {
     let btnId = $(this).data('id');
     let descInput = $(`#desc-form${btnId}`).val();
@@ -124,7 +79,51 @@ $(document).ready(function() {
       chefComments: chefInput,
     };
     updateRecipe(dataOBJ);
+  });
 
+  $.get('/api/userData').then( function(data){
+    // console.log('userdata', data.id);
+    usernameField.text(data.username);
+    firstnameField.text(data.firstName);
+    lastnameField.text(data.lastName);
+    bioField.text(data.bio);
+    return UserId = data.id;
+  });
+
+  // Add Recipe Form
+  addRecipeForm.on('submit', function(event) {
+    event.preventDefault();
+    let recipeData = {
+      recipeName: recipeNameInput.val().trim(),
+      ingredients:ingredientsInput.val().trim(),
+      instructions:instructionsInput.val().trim(),
+      description:descriptionInput.val().trim(),
+      chefComments:chefCommentsInput.val().trim(),
+    };
+    if (!recipeData.recipeName || !recipeData.ingredients || !recipeData.instructions || !recipeData.description) {
+      alert('please fill out the form completely');
+      return;
+    }
+    function addRecipe(recipeName, ingredients, instructions, description, chefComments) {
+      $.post('/api/add-recipe', {
+        recipeName: recipeName,
+        ingredients: ingredients,
+        instructions: instructions,
+        description: description,
+        chefComments: chefComments,
+      }).then(function(data) {
+        // reload page
+        console.log('profile.js');
+        window.location.replace('/profile');
+      }).catch(err => res.status(401).json(err));
+    }
+    addRecipe(recipeData.recipeName, recipeData.ingredients, recipeData.instructions, recipeData.description, recipeData.chefComments);
+    // reset form values to empty
+    recipeNameInput.val('');
+    ingredientsInput.val('');
+    instructionsInput.val('');
+    descriptionInput.val('');
+    chefCommentsInput.val('');
   });
 
 });
